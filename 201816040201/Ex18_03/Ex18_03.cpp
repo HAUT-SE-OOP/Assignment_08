3fe168987df0a944cc9337a023875fed95a99261
huatzy <56495508+huatzy@users.noreply.github.com>
2019-12-23 20:47:46 +0800

    Add files via upload

---
+++ 201816040201/Ex18_03/Ex18_03.cpp
@@ -0,0 +1,43 @@
+﻿
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+template<typename T>
+bool isEqual(T &a ,T  & b)
+{
+    if (a == b)
+    {
+        cout << "相等" << endl;
+        return true;
+    }
+    else
+    {
+        cout << "不同" << endl;
+        return false;
+    }
+}
+template<typename T>
+bool operator==(T & a,T &b)
+{
+    return a != b ? false : true;
+}
+
+
+int main()
+{
+    int a = 1, b = 2;
+    string x = "终了", y = x;
+    if(x==y)
+    {
+        cout << "相同" << endl;
+    }
+    else
+    {
+        cout << "不同" << endl;
+    }
+    isEqual(a, b);
+    isEqual(x, y);
+}
+
