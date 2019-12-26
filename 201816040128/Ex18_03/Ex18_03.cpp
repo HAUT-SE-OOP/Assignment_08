ed85d86f988a6c59ceda6137c38f2dc13e1f1d86
201816040128尚莹莹 <56549207+201816040128@users.noreply.github.com>
2019-12-26 17:44:01 +0800

    Add files via upload
    This is my aasiment
    student ID:201816040128

---
+++ 201816040128/Ex18_03/Ex18_03.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+bool isEqualTo(T &a, T &b)
+{
+	if (a == b)
+		return true;
+	if (a != b)
+		return false;
+}
+
+int main()
+{
+	int a = 1,  b = 1;
+	int c = 1, d = 2;
+	double e=1.1, f=1.1;
+	double g = 1.2, h = 2.1;
+	cout << "a=1, b=1 判断结果是： "<<isEqualTo(a, b)<<endl;
+	cout << "c=1, d=2 判断结果是： " << isEqualTo(c, d)<<endl;
+	cout << "e=1.1, f=1.1 判断结果是： " << isEqualTo(e, f)<<endl;
+	cout << "g=1.2, h=2.1 判断结果是： " << isEqualTo(g, h)<<endl;
+}
+
