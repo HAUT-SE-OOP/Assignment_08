1e7944a818d0fd7ba131bd978b9e2ffa426e7868
MisonoMayuBW <44313792+MisonoMayuBW@users.noreply.github.com>
2019-12-26 15:50:23 +0800

    Create Ex18_04.cpp

---
+++ 201816040327/Ex18_04/Ex18_04.cpp
@@ -0,0 +1,46 @@
+//
+//  Ex18_04.cpp
+//  Ex18_03
+//
+//  Created by misonomayubw on 2019/12/26.
+//  Copyright © 2019 misonomayubw. All rights reserved.
+//
+
+#include "Array.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Array <int> integers1(7)
+    Array integers2;
+    cout<<integers1.getSize();
+    cout<<integers2.getSize();
+    cin>>integers1>>integers2;
+
+    if(integers1 != integers2)
+        cout<<"integers1 and integers2 are not equal"<<endl;
+
+    Array <int>integers3(integers1)
+    cout<<integers3.getSize()<<integers3;
+
+    integers1=integers2;
+    cout<<"integers1:\n"<<integers1
+        <<"integers2:\n"<<integers2;
+
+    cout<<"\nEvaluating: equal"<< "\n" << endl;
+
+    if(integers1==integers2)
+    {
+        cout<<"equal"<<endl;
+    }
+     cout<<"\nintegers1[5] is "<<integers1[5];
+
+     cout<<"\n\nAssigning 1000 to integers1[5]"<<endl;
+     integers1[5]=1000;
+
+     cout<<"integers1:\n"<<integers1;
+
+    return 0;
+}
